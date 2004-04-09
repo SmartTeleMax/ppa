c10e9a27aec782d2bc39d3166b419ca0542a6e4f
Denis Otkidach <denis.otkidach@gmail.com>
2004-04-09 15:47:01 +0000

    Testing unicode templates

--- tests/testTemplate.py
+++ tests/testTemplate.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
-# $Id: testTemplate.py,v 1.14 2003/11/25 12:08:53 ods Exp $
+# -*- coding: utf-8 -*-
+# $Id: testTemplate.py,v 1.1.1.1 2004/04/09 13:18:11 ods Exp $
 
-import unittest, sys, os
+import unittest, sys, os, codecs
 from glob import glob
 from cStringIO import StringIO
 
 dir = os.path.dirname(os.path.abspath(globals().get('__file__', sys.argv[0])))
-sys.path.insert(0, os.path.dirname(os.path.dirname(dir)))
+sys.path.insert(0, '..')
 
 from PPA.Template.Controller import TemplateController
 from PPA.Template.SourceFinders import FileSourceFinder
@@ -14,28 +15,36 @@ from PPA.Template.SourceFinders import FileSourceFinder
 
 class TemplateTestCase(unittest.TestCase):
 
-    def __init__(self, template_name, template_type, globals, locals,
+    def __init__(self, template_name, template_type, charset, globals, locals,
                  templates_path, results_path):
         unittest.TestCase.__init__(self)
         self.template_name = template_name
         self.template_type = template_type
+        if charset is None:
+            self.reader = file
+            self.writer = StringIO
+        else:
+            self.reader = lambda fn: codecs.getreader(charset)(file(fn))
+            self.writer = lambda: codecs.getwriter(charset)(StringIO())
         self.globals = globals
         self.locals = locals
         self.templates_path = templates_path
         self.results_path = results_path
     
     def runTest(self):
-        source_finder = FileSourceFinder([self.templates_path])
+        source_finder = FileSourceFinder([self.templates_path],
+                                         file=self.reader)
         controller = TemplateController(source_finder=source_finder)
         template = controller.getTemplate(self.template_name,
                                           self.template_type)
-        fp = StringIO()
+        fp = self.writer()
         template.interpret(fp, self.globals, self.locals)
         got_result = fp.getvalue()
         # We use real (possibly auto-determined) type of template when looking
         # for result.
-        result_fp = open(os.path.join(self.results_path,
-                '.'.join((self.template_name, template.type))))
+        result_file_name = os.path.join(self.results_path,
+                                '.'.join((self.template_name, template.type)))
+        result_fp = file(result_file_name)
         result = result_fp.read()
         result_fp.close()
         self.assertEqual(got_result, result)
@@ -45,56 +54,82 @@ class TemplateTestSuite(unittest.TestSuite):
 
     def __init__(self, templates_path, results_path, list_of_tests):
         unittest.TestSuite.__init__(self)
-        for template_name, template_type, globals, locals in list_of_tests:
-            test = TemplateTestCase(template_name, template_type, globals,
-                                    locals, templates_path, results_path)
+        for template_name, template_type, charset, globals, locals \
+                in list_of_tests:
+            test = TemplateTestCase(template_name, template_type, charset,
+                                    globals, locals,
+                                    templates_path, results_path)
             self.addTest(test)
 
 
 _list_of_tests = [
-    ('test',  'pyem',  {'var': 'global'},
-                       {'var': 'local'}),
-    ('test',  None,    {'var': 'global'},
-                       {'var': 'local'}),
-    ('test2', 'htal',  {'var': 'global'},
-                       {'title': 'Just a title', 'var': 'local'}),
-    ('test2', None,    {'var': 'global'},
-                       {'title': 'Just a title', 'var': 'local'}),
-    ('test3', 'xtal',  {'var': 'global'},
-                       {'title': 'Just a title', 'var': 'local'}),
-    ('test3', None,    {'var': 'global'},
-                       {'title': 'Just a title', 'var': 'local'}),
-    ('test4', 'htal',  {},
-                       {'title': 'Just a title', 'body': 'Just a body'}),
-    ('test4', None,    {},
-                       {'title': 'Just a title', 'body': 'Just a body'}),
-    ('test5', 'dtml',  {'var': 'global'},
-                       {'title': 'Just a title', 'items': ['foo', 'bar'],
-                        'var': 'local'}),
-    ('test5', None,    {'var': 'global'},
-                       {'title': 'Just a title', 'items': ['foo', 'bar'],
-                        'var': 'local'}),
-    ('test6', 'raw',   {}, {}),
-    ('test6', None,    {}, {}),
-    ('test7', 'pysi',  {'var': 'global'}, 
-                       {'title': 'Just a title',
-                        'body': ['para1', 'para2', 'para3'],
-                        'max_paras': 2, 'var': 'local'}),
-    ('test7', None,    {'var': 'global'},
-                       {'title': 'Just a title',
-                        'body': ['para1', 'para2', 'para3'],
-                        'max_paras': 2, 'var': 'local'}),
-    ('test8', None,    {},
-                       {'sections': [
-                           {'title': 'Title1', 'children': []},
-                               {'title': 'Title2', 'children': [
-                               {'title': 'Title2.1', 'children': []},
-                               {'title': 'Title2.2', 'children': []}
-                       ]}]}),
-    ('test9', 'xhtal', {'var': 'global'},
-                       {'title': 'Just a title', 'var': 'local'}),
-    ('test9', None,    {'var': 'global'},
-                       {'title': 'Just a title', 'var': 'local'}),
+    ('test',   'pyem',  None,    {'var': 'global'},
+                                 {'var': 'local'}),
+    ('test',   None,    None,    {'var': 'global'},
+                                 {'var': 'local'}),
+    ('test2',  'htal',  None,    {'var': 'global'},
+                                 {'title': 'Just a title',
+                                  'var': 'local'}),
+    ('test2',  None,    None,    {'var': 'global'},
+                                 {'title': 'Just a title',
+                                  'var': 'local'}),
+    ('test3',  'xtal',  None,    {'var': 'global'},
+                                 {'title': 'Just a title',
+                                  'var': 'local'}),
+    ('test3',  None,    None,    {'var': 'global'},
+                                 {'title': 'Just a title',
+                                  'var': 'local'}),
+    ('test4',  'htal',  None,    {},
+                                 {'title': 'Just a title',
+                                  'body': 'Just a body'}),
+    ('test4',  None,    None,    {},
+                                 {'title': 'Just a title',
+                                  'body': 'Just a body'}),
+    ('test5',  'dtml',  None,    {'var': 'global'},
+                                 {'title': 'Just a title',
+                                  'items': ['foo', 'bar'],
+                                  'var': 'local'}),
+    ('test5',  None,    None,    {'var': 'global'},
+                                 {'title': 'Just a title',
+                                  'items': ['foo', 'bar'],
+                                  'var': 'local'}),
+    ('test6',  'raw',   None,    {}, {}),
+    ('test6',  None,    None,    {}, {}),
+    ('test7',  'pysi',  None,    {'var': 'global'}, 
+                                 {'title': 'Just a title',
+                                  'body': ['para1', 'para2', 'para3'],
+                                  'max_paras': 2, 'var': 'local'}),
+    ('test7',  None,    None,    {'var': 'global'},
+                                 {'title': 'Just a title',
+                                  'body': ['para1', 'para2', 'para3'],
+                                  'max_paras': 2, 'var': 'local'}),
+    ('test8',  None,    None,    {},
+                                 {'sections': [
+                                     {'title': 'Title1',
+                                      'children': []},
+                                     {'title': 'Title2',
+                                      'children': [{'title': 'Title2.1',
+                                                    'children': []},
+                                                   {'title': 'Title2.2',
+                                                    'children': []}]}
+                                 ]}),
+    ('test9',  'xhtal', None,    {'var': 'global'},
+                                 {'title': 'Just a title',
+                                  'var': 'local'}),
+    ('test9',  None,    None,    {'var': 'global'},
+                                 {'title': 'Just a title',
+                                  'var': 'local'}),
+    ('test9',  None,    None,    {'var': 'global'},
+                                 {'title': 'Just a title',
+                                  'var': 'local'}),
+    # Tests with charset='utf-8'
+    ('test10', 'raw',   'utf-8', {}, {}),
+    ('test11', 'pysi',  'utf-8', {'cyrillic': 'кириллица'.decode('utf-8')},
+                                 {}),
+    ('test12', 'pyem',  'utf-8', {'cyrillic': 'кириллица'.decode('utf-8')},
+                                 {}),
+    ('test13', 'htal',  'utf-8', {'cyrillic': 'кириллица'.decode('utf-8')},
+                                 {}),
 ]
 
 
