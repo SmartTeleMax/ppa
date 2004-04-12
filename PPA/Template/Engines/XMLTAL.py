477e79640ce480e385d51d1d86af1708c32b8581
Denis Otkidach <denis.otkidach@gmail.com>
2004-04-12 09:40:52 +0000

    Now it works when encoding is specified

--- PPA/Template/Engines/XMLTAL.py
+++ PPA/Template/Engines/XMLTAL.py
@@ -1,18 +1,30 @@
-# $Id: XMLTAL.py,v 1.3 2003/11/25 12:08:52 ods Exp $
+# $Id: XMLTAL.py,v 1.1.1.1 2004/04/09 13:18:11 ods Exp $
 
 import _TALCommon
-
-
-# XXX zLOG module is outside of TAL -- need some hacks.
-import sys, new
-zLOG = new.module('zLOG')
-zLOG.LOG = lambda *args: None
-zLOG.INFO = zLOG.PROBLEM = None
-sys.modules['zLOG'] = zLOG
-
+from TAL.TALParser import TALParser
+from TAL.XMLParser import XMLParser
+from TAL.TALGenerator import TALGenerator
+from TAL.TALDefs import XML_NS
 
 
 class Engine(_TALCommon.Engine):
 
     type = 'xtal'
-    from TAL.TALParser import TALParser as _parser_class
+    class _parser_class(TALParser):
+        def __init__(self, gen, encoding=None):
+            XMLParser.__init__(self, encoding=encoding)
+            self.gen = gen
+            self.nsStack = []
+            self.nsDict = {XML_NS: 'xml'}
+            self.nsNew = []
+    
+    def compileString(self, source, template_name, get_template):
+        cengine = _TALCommon.Compiler()
+        generator = TALGenerator(cengine, self._xml)
+        if isinstance(source, unicode):
+            parser = self._parser_class(generator, encoding='utf-8')
+            parser.parseString(source.encode('utf-8'))
+        else:
+            parser = self._parser_class(generator)
+            parser.parseString(source)
+        return parser.getCode()
