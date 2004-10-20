7dbcb8437101805f3c149c32c56748bb160ef9c4
Denis Otkidach <denis.otkidach@gmail.com>
2004-10-20 15:03:57 +0000

    Simple case with GET and query string

--- PPA/HTTP/Form.py
+++ PPA/HTTP/Form.py
@@ -1,42 +1,60 @@
-# $Id$
+# $Id: Form.py,v 1.1 2004/10/12 15:07:36 ods Exp $
 
 from weakref import WeakKeyDictionary
 from Errors import ClientError
+import urllib
 
 
 class Form(object):
 
     maxContentLength = 1048576
     requireContentLength = 1
+    keepBlankValues = 0
     
     __cache = WeakKeyDictionary()
     
     def __new__(cls, request):
         if not cls.__cache.has_key(request):
-            cls.__cache[request] = self = object.__new__(cls, request)
+            cls.__cache[request] = self = object.__new__(cls)
             self._init(request)
         return cls.__cache[request]
 
     def _init(self, request):
+        self._dict = {}
         method = request.method()
         if method in ('GET', 'HEAD'):
-            self.parseURLEncoded(request.query())
+            self.parseURLEncodedString(request.query())
         elif method=='POST':
             content_type = request.headers.get(
                         'Content-Type', 'application/x-www-form-urlencoded')
             try:
-                content_length = request.headers['Content-Length']
-            except KeyError:
-                if self.requireContentLength:
-                    raise ClientError(411, '')
-                else:
-                    content_length = self.maxContentLength
+                content_length = int(request.headers['Content-Length'])
+            except (KeyError, ValueError):
+                content_length = None
             else:
                 if content_length>self.maxContentLength:
                     raise ClientError(413, '')
+            if content_length is None and self.requireContentLength:
+                raise ClientError(411, '')
             if content_type=='application/x-www-form-urlencoded':
-                # XXX Silently stripping large body is a bad thing.
-                # Should try reading maxContentLength+1 byte to check!
-                self.parseURLEncoded(request.read(content_length))
+                self.parseURLEncoded(request, content_length)
             elif content_type=='multipart/form-data':
-                self.parseMultipart(request)
+                self.parseMultipart(request, content_length)
+    
+    def parseURLEncoded(self, request, content_length=None):
+        if content_length is None:
+            body = request.read(self.maxContentLength+1)
+            if len(body)>self.maxContentLength:
+                raise ClientError(413, '')
+        else:
+            body = request.read(content_length)
+        self.parseURLEncodedString(body)
+
+    def parseURLEncodedString(self, body):
+        pairs = [(ps.split('=', 1)+[''])[:2] for ps0 in body.split(';')
+                                              for ps in ps0.split('&')]
+        unquote = urllib.unquote_plus
+        setdefault = self._dict.setdefault
+        for qfn, qfv in pairs:
+            if qfv or self.keepBlankValues:
+                setdefault(unquote(qfn), []).append(unquote(qfv))
---
+++ tests/testHTTPForm.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# $Id$
+
+import unittest, sys, os
+
+dir = os.path.dirname(os.path.abspath(globals().get('__file__', sys.argv[0])))
+sys.path.insert(0, os.path.dirname(dir))
+
+from PPA.HTTP.CGI import Adapter
+from PPA.HTTP.Form import Form
+from cStringIO import StringIO
+
+
+import logging
+logging.basicConfig()
+
+
+class FormTest(unittest.TestCase):
+
+    class CGIApp(Adapter):
+        def handle(self, request, response):
+            form = Form(request)
+            self.form_dict = form._dict
+    
+    def runCGI(self, method='GET', query_string=None, content_type=None,
+               data=''):
+        environ = {
+            'REQUEST_METHOD': method,
+            'SCRIPT_NAME': '/',
+        }
+        orig_environ = os.environ.copy()
+        orig_stdin = sys.stdin
+        orig_stdout = sys.stdout
+        sys.stdout = StringIO()
+        if data:
+            sys.stdin = StringIO(data)
+            environ['CONTENT_LENGTH'] = len(data)
+        if query_string:
+            environ['QUERY_STRING'] = query_string
+        os.environ.update(environ)
+        app = self.CGIApp()
+        app()
+        sys.stdout = orig_stdout
+        sys.stdin = orig_stdin
+        for key in environ.keys():
+            del os.environ[key]
+        os.environ.update(orig_environ)
+        return app.form_dict
+    
+    def testGET(self):
+        self.assertEqual(self.runCGI(query_string='a=1&b=2'),
+                         {'a': ['1'], 'b': ['2']})
+
+
+def testSuite():
+
+    return unittest.makeSuite(FormTest)
+
+
+if __name__=='__main__':
+    unittest.TextTestRunner().run(testSuite())
