e62b82497815bf2afd15b16ac249c8f9924b71dd
Denis Otkidach <denis.otkidach@gmail.com>
2006-10-18 11:28:18 +0000

    WSGI adapter

---
+++ PPA/HTTP/WSGI.py
@@ -0,0 +1,41 @@
+import Base, CGI, logging
+logger = logging.getLogger(__name__)
+
+
+class Request(CGI.Request):
+
+    def scheme(self):
+        try:
+            return self._environ['wsgi.url_scheme']
+        except KeyError:
+            return CGI.Request.scheme(self)
+
+
+class WSGIFileObject:
+
+    def write(self, *args, **kwargs):
+	raise RuntimeError('WSGI start_response was not called yet')
+
+
+class Response(CGI.Response):
+    def __init__(self, request, start_response, buffered=1):
+	self._start_response = start_response
+	CGI.Response.__init__(self, request, WSGIFileObject(), buffered) # fp is None, all fp method redefined
+
+    def _write_status(self):
+	# All work is done by _write_headers
+	pass
+
+    def _write_headers(self):
+	status = "%d %s" % (self._status,
+                            self.statusCodes.get(self._status, 'Unknown'))
+	self._fp.write = self._start_response(status, self.headers._headers)
+	
+
+class Adapter(Base.Adapter):
+
+    def __call__(self, environ, start_response):
+        request = Request(environ, environ['wsgi.input'])
+        response = Response(request, start_response, buffered=1)	        
+        Base.Adapter.__call__(self, request, response)
+	return []
