f0b4da1c653b4a5e3f6fca63dcc1581e756ee774
Denis Otkidach <denis.otkidach@gmail.com>
2006-10-24 07:03:36 +0000

    Bugfix: use passed optional argument with engine types

--- PPA/Template/Engines/__init__.py
+++ PPA/Template/Engines/__init__.py
@@ -1,4 +1,4 @@
-# $Id: __init__.py,v 1.3 2003/11/25 12:08:52 ods Exp $
+# $Id: __init__.py,v 1.1.1.1 2004/04/09 13:18:11 ods Exp $
 
 __all__ = ['enginesByType', 'UnknownTemplateType', 'EngineImporter']
 
@@ -11,8 +11,8 @@ class UnknownTemplateType(LookupError): pass
 
 class EngineImporter:
 
-    def __init__(self, engines_by_type = enginesByType):
-        self._engines_by_type = enginesByType
+    def __init__(self, engines_by_type=enginesByType):
+        self._engines_by_type = engines_by_type
     
     def __call__(self, template_type):
         '''Return module of engine by its type'''
