05e9c458c4b3da5b6f2776ebe9e3486cb2bab212
Denis Otkidach <denis.otkidach@gmail.com>
2006-12-14 14:02:43 +0000

    Added template name in errors. XXX lineno is wrong yet.

--- PPA/Template/Engines/PySI.py
+++ PPA/Template/Engines/PySI.py
@@ -1,16 +1,18 @@
-# $Id: PySI.py,v 1.1.1.1 2004/04/09 13:18:11 ods Exp $
+# $Id: PySI.py,v 1.2 2005/11/17 23:14:12 corva Exp $
 
 '''Python-style string interpolation'''
 
 
 class EvalDict:
 
-    def __init__(self, globals, locals):
+    def __init__(self, globals, locals, template_name='?'):
+        self.template_name = template_name
         self.globals = globals
         self.locals = locals
 
     def __getitem__(self, key):
-        return eval(key, self.globals, self.locals)
+        code = compile(key, self.template_name, 'eval')
+        return eval(code, self.globals, self.locals)
 
 
 class Engine:
@@ -18,16 +20,19 @@ class Engine:
     type = 'pysi'
 
     def compileString(self, source, template_name, get_template):
-        return source
+        return template_name, source
 
     def compileFile(self, fp, template_name, get_template):
-        return fp.read()
+        return template_name, fp.read()
 
     def interpret(self, program, fp, globals, locals, get_template):
-        fp.write(program % EvalDict(globals, locals))
+        template_name, source = program
+        fp.write(source % EvalDict(globals, locals, template_name))
 
     def dump(self, program):
-        return program
+        template_name, source = program
+        return '\0'.join([template_name, source])
 
     def load(self, s):
-        return s
+        template_name, source = s.split('\0', 1)
+        return template_name, source
